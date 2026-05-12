c584eea853e8f195c2a0ec9f70f51f8694e27891
r3dbars <r3dbars@users.noreply.github.com>
2026-05-12 15:36:59 -0500

    Harden diagnostics runtime gate

--- script/smoke_test.sh
+++ script/smoke_test.sh
@@ -22,6 +22,7 @@ swift test
 ./script/check_score_targets_self_test.sh
 ./script/check_prompt_app_proof_self_test.sh
 ./script/check_prompt_app_manifest_proof_self_test.sh
+./script/check_diagnostics_log_self_test.sh
 ./script/check_proof_manifest_self_test.sh
 ./script/check_visual_placement_evidence_self_test.sh
 ./script/check_visual_placement_evidence.sh
