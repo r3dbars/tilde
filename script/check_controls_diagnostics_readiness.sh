2a58d4ad53ed76879c7e9574ed578c273ddb1811
r3dbars <r3dbars@users.noreply.github.com>
2026-05-12 17:24:05 -0500

    Collapse controls readiness checks

--- script/check_controls_diagnostics_readiness.sh
+++ script/check_controls_diagnostics_readiness.sh
@@ -8,12 +8,17 @@ failures=0
 proof_output="$(mktemp -d)"
 LOG_DIR="$(mktemp -d)"
 SWIFT_TEST_ARGS=()
-CURRENT_BUILD_ENV=(AUTOCOMPLETE_LAB_PRIVACY_PROOF_OUTPUT="$proof_output")
+SWIFT_TEST_ARGS_CONFIGURED=0
+CURRENT_BUILD_ENV=(
+  AUTOCOMPLETE_LAB_PRIVACY_PROOF_OUTPUT="$proof_output"
+  AUTOCOMPLETE_LAB_REBUILD_PRIVACY_PROOF=1
+)
 
 if [[ -n "${AUTOCOMPLETE_LAB_READINESS_SCRATCH_PATH:-}" ]]; then
   SWIFT_SCRATCH_PATH="$AUTOCOMPLETE_LAB_READINESS_SCRATCH_PATH"
   mkdir -p "$SWIFT_SCRATCH_PATH"
   SWIFT_TEST_ARGS=(--scratch-path "$SWIFT_SCRATCH_PATH/swift-tests")
+  SWIFT_TEST_ARGS_CONFIGURED=1
   CURRENT_BUILD_ENV+=(AUTOCOMPLETE_LAB_SWIFT_SCRATCH_PATH="$SWIFT_SCRATCH_PATH/current-build")
 fi
 
@@ -63,10 +68,10 @@ require_executable() {
   fi
 }
 
-run_swift_test() {
+run_swift_tests() {
   local filter="$1"
 
-  if ((${#SWIFT_TEST_ARGS[@]})); then
+  if [[ "$SWIFT_TEST_ARGS_CONFIGURED" == "1" ]]; then
     swift test "${SWIFT_TEST_ARGS[@]}" --filter "$filter"
   else
     swift test --filter "$filter"
@@ -82,21 +87,14 @@ for script_path in \
   run_check "Executable $(basename "$script_path")" require_executable "$script_path" || failures=$((failures + 1))
 done
 
-for filter in \
-  SettingsWindowControllerStateTests \
-  DiagnosticsWindowControllerStateTests \
-  SuggestionControlPolicyTests \
-  SuggestionPauseSchedulePolicyTests \
-  DisabledAppSelectionTests \
-  RawTracePrivacyExpiryTests \
-  RawTraceReportExportTests \
-  PrivacyExportProofCommandTests; do
-  run_logged_check "Swift test $filter" run_swift_test "$filter" || failures=$((failures + 1))
-done
+CONTROL_FILTER='SettingsWindowControllerStateTests|DiagnosticsWindowControllerStateTests|SuggestionControlPolicyTests|SuggestionPauseSchedulePolicyTests|DisabledAppSelectionTests|RawTracePrivacyExpiryTests|RawTraceReportExportTests|PrivacyExportProofCommandTests'
+run_logged_check "Swift controls and diagnostics tests" run_swift_tests "$CONTROL_FILTER" || failures=$((failures + 1))
 
 run_logged_check "Delete local traces self-test" ./script/delete_local_traces_self_test.sh || failures=$((failures + 1))
 run_logged_check "Diagnostics log self-test" ./script/check_diagnostics_log_self_test.sh || failures=$((failures + 1))
-run_logged_check "Redacted report export" ./script/check_redacted_report_export.sh || failures=$((failures + 1))
+run_logged_check "Redacted report export" env \
+  AUTOCOMPLETE_LAB_SWIFT_SKIP_BUILD=1 \
+  ./script/check_redacted_report_export.sh || failures=$((failures + 1))
 
 run_logged_check "Current build privacy export proof" env \
   "${CURRENT_BUILD_ENV[@]}" \
