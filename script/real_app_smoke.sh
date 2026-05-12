31761e75b64d56ffa49a3bc7360565c7dad68b3e
r3dbars <r3dbars@users.noreply.github.com>
2026-05-12 17:56:26 -0500

    Bound TextEdit open fallback

--- script/real_app_smoke.sh
+++ script/real_app_smoke.sh
@@ -3862,7 +3862,8 @@ open_textedit_smoke_document() {
     return 0
   fi
 
-  osascript - "$file_path" <<'APPLESCRIPT' >/dev/null 2>&1 || true
+  (
+    osascript - "$file_path" <<'APPLESCRIPT'
 on run argv
   set targetPath to item 1 of argv
   tell application "TextEdit"
@@ -3871,6 +3872,9 @@ on run argv
   end tell
 end run
 APPLESCRIPT
+  ) >/dev/null 2>&1 &
+  local open_osascript_pid="$!"
+  wait_for_background_process "$open_osascript_pid" 4 "TextEdit AppleScript open" >/dev/null 2>&1 || true
 
   if wait_for_textedit_document_open "$window_title" 6; then
     return 0
