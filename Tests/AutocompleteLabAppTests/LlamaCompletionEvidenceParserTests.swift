06acd95e259840128d280a3c17f407a9de9228b2
r3dbars <r3dbars@users.noreply.github.com>
2026-08-17 12:03:02 -0500

    Simplify parser JSON fixtures

--- Tests/AutocompleteLabAppTests/LlamaCompletionEvidenceParserTests.swift
+++ Tests/AutocompleteLabAppTests/LlamaCompletionEvidenceParserTests.swift
@@ -6,9 +6,10 @@ import Testing
 struct LlamaCompletionEvidenceParserTests {
     @Test("Decodes greedy token log probabilities and alternatives")
     func decodesProbabilities() throws {
-        let json = #"{"content":" hello there","probs":[{"token":" hello","logprob":-0.2231435513,"top_logprobs":[{"token":" hello","logprob":-0.2231435513},{"token":" hey","logprob":-1.6094379124}]},{"token":" there","logprob":-0.5108256238,"top_logprobs":[]}]}"#
-        let corrected = json.replacingOccurrences(of: "\\\"", with: "\"")
-        let decoded = try LlamaCompletionEvidenceParser.decode(Data(corrected.utf8))
+        let json = """
+        {"content":" hello there","probs":[{"token":" hello","logprob":-0.2231435513,"top_logprobs":[{"token":" hello","logprob":-0.2231435513},{"token":" hey","logprob":-1.6094379124}]},{"token":" there","logprob":-0.5108256238,"top_logprobs":[]}]}
+        """
+        let decoded = try LlamaCompletionEvidenceParser.decode(Data(json.utf8))
         #expect(decoded.content == " hello there")
         #expect(decoded.tokens.count == 2)
         #expect(abs(decoded.tokens[0].probability - 0.8) < 0.0001)
@@ -19,7 +20,7 @@ struct LlamaCompletionEvidenceParserTests {
 
     @Test("Missing probability trace fails open")
     func missingProbabilities() throws {
-        let json = #"{"content":"hello"}"#.replacingOccurrences(of: "\\\"", with: "\"")
+        let json = """{"content":"hello"}"""
         let decoded = try LlamaCompletionEvidenceParser.decode(Data(json.utf8))
         #expect(decoded.content == "hello")
         #expect(decoded.tokens.isEmpty)
